4fd3f7531578f2a43ad4bdcafb00f04705492872
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2023-08-06 13:30:22 +0300

    new commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// typed js
+
+const typed = new Typed ('.change-content',{
+    strings: ['Promote collaboration','Transform your Business','Boost Profits','Empower your Team','Gain an Edge'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
