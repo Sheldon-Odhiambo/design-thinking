304f1dc57b7516346b74deb8fefe8d03c85ecffa
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2023-08-31 00:27:48 +0300

    chore

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const typed = new Typed ('.change-content',{
     backDelay: 1000,
     loop: true
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contact-form");
+    
+    form.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+      // You can add your logic here to process the form submission
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    const challenge = form.elements.challenge.value;
+    const consent = form.elements.consent.checked;
+
+      // Example: You can send this data to a server using AJAX or fetch API
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Challenge:", challenge);
+    console.log("Consent:", consent);
+
+      // You can also display a success message or perform other actions
+    alert("Form submitted successfully!");
+    form.reset();
+    });
+});
+
