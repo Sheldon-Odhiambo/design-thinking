89c00b9d555cefbb80a51d91e1634b3df2c305fc
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2023-09-02 23:24:19 +0300

    feat

--- script.js
+++ script.js
@@ -8,30 +8,6 @@ const typed = new Typed ('.change-content',{
     loop: true
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("contact-form");
-    
-    form.addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-      // can add your logic here to process the form submission
-    const name = form.elements.name.value;
-    const email = form.elements.email.value;
-    const challenge = form.elements.challenge.value;
-    const consent = form.elements.consent.checked;
-
-      // Example: You can send this data to a server using AJAX or fetch API
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Challenge:", challenge);
-    console.log("Consent:", consent);
-
-      // You can also display a success message or perform other actions
-    alert("Form submitted successfully!");
-    form.reset();
-    });
-});
-
 
 // upcoming events 
 
@@ -62,4 +38,98 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // upcoming events end
+
+
+
+
+// your challange starts 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent the form from actually submitting
+
+      // Get form field values
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const challenge = document.querySelector("#challenge").value;
+    const consent = document.querySelector("#consent").checked;
+
+      // Validate consent
+    if (!consent) {
+        alert("You must consent to share your challenge.");
+        return;
+    }
+
+      // Display submitted data
+    const message =
+        `Name: ${name}\n` +
+        `Email: ${email}\n` +
+        `Challenge: ${challenge}`;
+
+    alert("Submitted data:\n\n" + message);
+
+      // Reset the form
+    form.reset();
+    });
+});
+
+
+
+// your challange ends 
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.post("/submit", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const challenge = req.body.challenge;
+  const consent = req.body.consent;
+
+  if (!consent) {
+    res.status(400).send("You must consent to share your challenge.");
+    return;
+  }
+
+  // Configure Nodemailer
+  const transporter = nodemailer.createTransport({
+    service: "YourEmailService", // e.g., "Gmail", "Outlook"
+    auth: {
+      user: "your@email.com",
+      pass: "your-password"
+    }
+  });
+
+  const mailOptions = {
+    from: "your@email.com",
+    to: "oumashelly004@gmail.com",
+    subject: "New Challenge Submission",
+    text: `Name: ${name}\nEmail: ${email}\nChallenge: ${challenge}`
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error sending email:", error);
+      res.status(500).send("Error sending email.");
+    } else {
+      console.log("Email sent:", info.response);
+      res.status(200).send("Form data submitted and email sent!");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
